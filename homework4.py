9a2acb3db8e7f7c5d6c6ecf9603c2ea91e12830e
Lufanin <orion.pax37@gmail.com>
2025-01-06 04:55:38 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,16 @@
+# Задача 1 [2, 2, 2]
+# Задача 2 [1, 2, 2]
+# Задача 3 4.2
+# n = 5
+# my_list = [2, 3, 1, 5, 10]
+# print(sum(my_list)/len(my_list))
+# Задача 4
+# n = int(input()) 10
+# m = int(input()) 5
+# list_1 = [10, 3, 1, 10, 23, 32, 18, 74, 29, 73, 1, 82]
+# result = list_1[m]
+# print(result) 32
+# Задача 5
+# n = int(input())
+# list_2 = [6, 1, 9, 2, 8, 3, 7]
+# print(sum(list_2[:1:])) 6
